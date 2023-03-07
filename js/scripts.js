764a56b610969ab6092be022ee963f035a9f33d0
Jajowski <tamojajanidze26@gmail.com>
2023-03-07 19:32:54 +0400

    saboloo proekti

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let gaperadeba = () => { 
+    document.getElementById("poto").scr = "img/peradi.png" 
+     
+
+}
+
+let gauperuleba = () => {
+    document.getElementById("poto").scr = "img/shopping-cart.png"
+}
